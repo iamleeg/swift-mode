25dc7431f2128bd3af025d6106161013530e2a9d
Cameron Desautels <camdez@gmail.com>
2014-06-03 18:56:41 -0500

    Add understanding of comments

--- swift-mode.el
+++ swift-mode.el
@@ -1,5 +1,17 @@
 ;;; swift-mode.el --- Major mode for editing Swift files
 
+(defgroup swift nil
+  "Major mode for editing Swift code."
+  :prefix "swift-"
+  :group 'languages)
+
+(defcustom swift-comment-column 32
+  "Indentation column of comments."
+  :type 'integer :group 'swift)
+
+;; Safe file variables
+(put 'swift-comment-column 'safe-local-variable 'integerp)
+
 (setq swift-keywords
       '("break" "class" "continue" "default" "do" "else" "for" "func" "if" "import" "in" "let" "return" "self" "struct" "super" "switch" "unowned" "var" "weak" "while"))
 
@@ -26,6 +38,9 @@
 
 (defvar swift-mode-syntax-table
   (let ((table (make-syntax-table)))
+    (modify-syntax-entry ?/ ". 124b" table)
+    (modify-syntax-entry ?* ". 23" table)
+    (modify-syntax-entry ?\n "> b" table)
     table)
   "Syntax table for Swift files.")
 
@@ -34,9 +49,14 @@
 
 ;;;###autoload
 (define-derived-mode swift-mode prog-mode "Swift"
-   "Major mode for editing Swift files.
+  "Major mode for editing Swift files.
 
 \\{swift-mode-map}"
-   (setq font-lock-defaults '((swift-font-lock-keywords))))
+  (setq font-lock-defaults '((swift-font-lock-keywords)))
+  (set (make-local-variable 'comment-start) "/* ")
+  (set (make-local-variable 'comment-end)   " */")
+  (set (make-local-variable 'comment-start-skip) "\\(//+\\|/\\*+\\)\\s *")
+  (set (make-local-variable 'comment-column) swift-comment-column)
+  (set (make-local-variable 'comment-use-syntax) t))
 
 (provide 'swift-mode)
